c4c3d3cda461771ad1c1fa7f82d5222b3a50add3
Darius Strasel <strasel.darius@gmail.com>
2017-03-12 13:12:32 -0400

    Solved in JavaScript.

--- Day 2/main.js
+++ Day 2/main.js
@@ -1,3 +1,29 @@
 /**
  * Created by dariusstrasel on 3/12/17.
  */
+/*
+'use strict';
+
+var _input = '';
+var _index = 0;
+process.stdin.on('data', (data) => { _input += data; });
+process.stdin.on('end', () => {
+	_input = _input.split(new RegExp('[\n ]+'));
+	main(+(_input[0]), +(_input[1]), +(_input[2]));
+});
+*/
+
+function main(mealCost, tipPercent, taxPercent) {
+// My Solution:
+    // Write your code here
+    var mealCost;
+    var tipPercent;
+    var taxPercent;
+
+    var tip = mealCost * (tipPercent / 100);
+    var tax = mealCost * (taxPercent / 100);
+
+    var totalCost = Number(mealCost + tip + tax).toPrecision(2);
+    // Use console.log() to print to stdout
+    console.log("The total meal cost is " + totalCost + " dollars.");
+}
