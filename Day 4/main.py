b048d0674297c9efba3a6a1fb96d229b559eca69
Darius Strasel <strasel.darius@gmail.com>
2017-03-13 19:33:50 -0400

    Finished class challenge in Python.

--- Day 4/main.py
+++ Day 4/main.py
@@ -63,12 +63,25 @@ Because , our code should print that the person is old. Three years pass and the
 The extra line at the end of the output is supposed to be there and is trimmed before being compared against the test case's expected output. If you're failing this challenge, check your logic and review your print statements for spelling errors."""
 
 class Person:
-    def __init__(self,initialAge):
+    def __init__(self, initialAge):
         # Add some more code to run some checks on initialAge
+        error = "Age is not valid, setting age to 0."
+        if type(initialAge) != 'int' and initialAge < 0:
+            print(error)
+            self.age = 0
+        else:
+            self.age = initialAge
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
+        if self.age < 13:
+            print("You are young.")
+        elif self.age >= 13 and self.age < 18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
     def yearPasses(self):
         # Increment the age of the person in here
+        self.age += 1
 
 # Following code is unchangeable on HackerRank:
 t = int(input())
