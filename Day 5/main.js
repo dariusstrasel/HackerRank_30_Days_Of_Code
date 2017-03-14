26b1b949067c0100eeee79b01736a8da12ddac84
Darius Strasel <strasel.darius@gmail.com>
2017-03-14 10:20:26 -0400

    Solved Day 5 for Python and JavaScript.

---
+++ Day 5/main.js
@@ -0,0 +1,31 @@
+/**
+ * Created by dariusstrasel on 3/14/17.
+ */
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    for (var index = 1; index < 11; index++){
+        console.log(n + " x " + index + " = " + (n * index))
+    }
+}
