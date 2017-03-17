c8a3b2699c3fdabe6cb13ca3bf3d681cf586b9cb
Dstrasel <siruku@gmail.com>
2017-03-17 11:15:47 -0400

    Solved reverse array for Python.

---
+++ Day 7 Arrays/main.py
@@ -0,0 +1,41 @@
+"""Objective
+Today, we're learning about the Array data structure. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+
+Input Format
+
+The first line contains an integer,  (the size of our array).
+The second line contains  space-separated integers describing array 's elements.
+
+Constraints
+
+, where  is the  integer in the array.
+Output Format
+
+Print the elements of array  in reverse order as a single line of space-separated numbers.
+
+Sample Input
+
+4
+1 4 3 2
+Sample Output
+
+2 3 4 1"""
+
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+def main(n , args):
+    if n > 0:
+        reversed_array = " ".join([str(args[index]) for index in reversed(range(0, len(args)))])
+        print(reversed_array)
+
+
+main(n, arr)
