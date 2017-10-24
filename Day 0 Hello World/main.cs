9c3e38421cca72b26977696c3038428034601ee6
Darius Strasel <strasel.darius@gmail.com>
2017-10-23 23:16:37 -0400

    Add .NET solution files for compiling C#.

--- Day 0 Hello World/main.cs
+++ Day 0 Hello World/main.cs
@@ -4,11 +4,21 @@ namespace ConsoleApplication.Day_0_Hello_World
 {
     class Solution
     {
-        public static void Main(String[] args)
+        public static void _Main(String[] args)
         {
             // Declare a variable named 'inputString' to hold our input.
             String inputString;
-
+            var result = new System.Collections.Generic.List<int>(){1, 2, 3};
+            
+            int integer = 12;
+            for (int i = 1; i < integer + 1; i++)
+            {
+                if (integer % 5 == 1)
+                {
+                    Console.WriteLine(true);
+                }
+            }
+            
             // Read a full line of input from stdin (cin) and save it to our variable, input_string.
             inputString = Console.ReadLine();
 
